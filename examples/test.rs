fb17086e0cfa4e85ac2daa5c19300fd6ed780ffc
nubiv <h.horace0921@gmail.com>
2025-09-02 15:37:08 +0800

    chore: fmt

--- examples/test.rs
+++ examples/test.rs
@@ -2,7 +2,7 @@ use tracing::info;
 
 fn main() {
     quantx_core::trace::init_tracing_stdout();
-    // let _guard = quantx_core::trace::init_with_persistence();
+    // let _guard = quantx_core::trace::init_tracing_file_persistence();
     let name = "QUANTX-ML";
 
     info!(target: "Debug", ?name, "Starting up...");
